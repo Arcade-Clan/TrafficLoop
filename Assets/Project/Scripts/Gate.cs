b05d7479e26155ac50adbaf0ecac7538d6bc57a4
Bahadır Yerdelen <bahadir@arcadeclan.io>
2022-10-10 09:50:05 +0300

    - Gate is integrated

--- Assets/Project/Scripts/Gate.cs
+++ Assets/Project/Scripts/Gate.cs
@@ -1,14 +1,30 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class Gate : MonoBehaviour
 {
 
+    public Transform model;
+    public Ease gateOpenEase = Ease.OutElastic;
+    public Ease gateCloseEase = Ease.OutElastic;
+    public float openTime = 1;
+    public float closeTime = 1;
+    
     void OnTriggerEnter(Collider other)
     {
         GameManager.Instance.IncreaseMoney(other.transform.parent.GetComponent<Car>(),transform.position+Vector3.up);
+        DOTween.Kill(model);
+        model.DORotate(new Vector3(0,180,0), openTime).SetEase(gateOpenEase);
     }
 
+    void OnTriggerExit(Collider other)
+    {
+        GameManager.Instance.IncreaseMoney(other.transform.parent.GetComponent<Car>(), transform.position + Vector3.up);
+        DOTween.Kill(model);
+        model.DORotate(new Vector3(0, 90, 0), closeTime).SetEase(gateCloseEase);
+    }
+    
 }

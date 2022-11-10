a92ac4104c04dc55c1cdce5bd404d8dddab4a6c7
BahadirArcadeClan <bahadir@arcadeclan.io>
2022-11-10 19:43:39 +0300

    - Road animations

--- Assets/Project/Scripts/Managers/LM.cs
+++ Assets/Project/Scripts/Managers/LM.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using DG.Tweening;
 using UnityEngine;
 using UnityExtensions;
@@ -21,7 +23,28 @@ public class LM : MonoSingleton<LM>
             GM.Instance.cam.transform.position = level.sections[levelIndexes[1]].cam.position;
             GM.Instance.cam.transform.rotation = level.sections[levelIndexes[1]].cam.rotation;
         }
+
+        ProcessTaggedObjects();
         UpdateGates();
+    }
+
+    public void ProcessTaggedObjects()
+    {
+        List<GameObject> roads = GameObject.FindGameObjectsWithTag("Road").ToList();
+        roads = roads.OrderBy(p => p.transform.position.x+p.transform.position.z).ToList();
+        for (int a = 0; a < roads.Count; a++)
+            Place(roads[a].transform,a);
+    }
+    
+    private void Place(Transform road,int index)
+    {
+        road.tag = "Untagged";
+        Vector3 position = road.localPosition;
+        Vector3 scale = road.localScale;
+        road.localScale = Vector3.zero;
+        road.localPosition += Vector3.up * 10;
+        road.DOScale(scale,0.5f).SetDelay(index/20f).SetEase(Ease.OutBack);
+        road.DOLocalMoveY(position.y, 0.5f).SetDelay(index/20f);
     }
     
     public void UpdateGates()
@@ -29,7 +52,7 @@ public class LM : MonoSingleton<LM>
         for (int a = 0; a < gates.Length; a++)
         {
             if(GM.Instance.upgrades[2].upgradeLevel>=a)
-                gates[a].Show();
+                gates[a].Hide();
             else
                 gates[a].Hide();
         }  
@@ -72,6 +95,7 @@ public class LM : MonoSingleton<LM>
         }
         
         GM.Instance.trafficController.RecalculateTrafficElements();
+        ProcessTaggedObjects();
         if(level.sections[levelIndexes[1]].cam)
         {
             level.sections[levelIndexes[1]].cam.Hide();

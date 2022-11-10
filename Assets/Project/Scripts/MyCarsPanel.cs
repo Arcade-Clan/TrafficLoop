eb002c7dbe643f17f59f613eba1b3ab14678087e
BahadirArcadeClan <bahadir@arcadeclan.io>
2022-11-10 18:49:34 +0300

    - Car locks are aded

--- Assets/Project/Scripts/MyCarsPanel.cs
+++ Assets/Project/Scripts/MyCarsPanel.cs
@@ -4,16 +4,16 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityExtensions;
 
 public class MyCarsPanel : MonoBehaviour
 {
    [Serializable]
    public class ImageClass
    {
-
-      public Sprite[] carSprite;
-      public Image carImage;
+      public Image[] carImages;
       public TextMeshProUGUI carAmount;
+      public TextMeshProUGUI carValue;
    }
 
    public ImageClass[] carImages;
@@ -28,13 +28,22 @@ public class MyCarsPanel : MonoBehaviour
    {
       while (true)
       {
-         for (int a = 0; a < carImages.Length; a++)
+         bool opened = false;
+         for (int a = carImages.Length - 1; a >= 0; a--)
          {
-            if (GM.Instance.cars[a].carLevel > 0)
-               carImages[a].carImage.sprite = carImages[a].carSprite[1];
+            if (GM.Instance.cars[a].carLevel > 0||opened)
+            {
+               opened = true;
+               carImages[a].carImages[1].Hide();
+               carImages[a].carImages[0].Show();
+            }
             else
-               carImages[a].carImage.sprite = carImages[a].carSprite[0];
+            {
+               carImages[a].carImages[0].Hide();
+               carImages[a].carImages[1].Show();
+            }
             carImages[a].carAmount.text = "" + GM.Instance.cars[a].carLevel;
+            carImages[a].carValue.text = "$" + GM.Instance.cars[a].carValue;
          }
 
          yield return null;

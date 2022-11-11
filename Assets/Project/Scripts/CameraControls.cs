017ddf949caa2c45fd232dc18706b8d961c0b645
Bahadır Yerdelen <bahadir@arcadeclan.io>
2022-11-11 11:10:16 +0300

    - Camera controls, its in camera

---
+++ Assets/Project/Scripts/CameraControls.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityExtensions;
+
+public class CameraControls : MonoBehaviour
+{
+
+    public Vector2 cameraFieldOfViewLimits;
+    public float slideSpeed=1;
+    public float zoomSpeed=1;
+    public float dragActivationPixels = 30;
+    public string state = "None";
+    public Vector2 position;
+    
+    void Start()
+    {
+        InputPanel.Instance.OnPointerDownFullInfo.AddListener(Down);
+        InputPanel.Instance.OnDragFullInfo.AddListener(Drag);
+        InputPanel.Instance.OnPointerUpFullInfo.AddListener(Up);
+    }
+
+
+    void Down(PointerEventData data)
+    {
+        if (state == "None")
+        {
+            state = "Listening";
+            position = data.position;
+        }
+    }
+
+    void Drag(PointerEventData data)
+    {
+        if (state == "Listening")
+        {
+            if (dragActivationPixels < Mathf.Abs(position.x - data.position.x))
+            {
+                state = "Horizontal";
+            }
+
+            else if (dragActivationPixels < Mathf.Abs(position.y - data.position.y))
+            {
+                state = "Vertical";
+            }   
+        }
+        
+        if (state == "Horizontal")
+            transform.parent.localEulerAngles += new Vector3(0, data.delta.x* slideSpeed, 0);
+        else if (state == "Vertical")
+            GetComponent<Camera>().fieldOfView = Mathf.Clamp(GetComponent<Camera>().fieldOfView + data.delta.y * zoomSpeed,
+                cameraFieldOfViewLimits.x, cameraFieldOfViewLimits.y);
+    }
+
+    void Up(PointerEventData data)
+    {
+        state = "None";
+    }
+    
+    
+}

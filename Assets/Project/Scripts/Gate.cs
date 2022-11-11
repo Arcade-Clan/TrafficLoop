31844ce7519fb2aeb2930a3f7306546520a448b5
Bahadır Yerdelen <bahadir@arcadeclan.io>
2022-11-11 14:14:01 +0300

    # Gate Initial Spawn

--- Assets/Project/Scripts/Gate.cs
+++ Assets/Project/Scripts/Gate.cs
@@ -21,8 +21,8 @@ public class Gate : MonoBehaviour
         Vector3 scale = model.localScale;
         model.localScale = Vector3.zero;
         model.localPosition+=Vector3.up*5;
-        model.DOScale(scale,1.5f*Random.Range(0.8f,1.2f)).SetEase(Ease.OutElastic).SetUpdate(UpdateType.Normal, true);
-        model.DOLocalMoveY(position.y, 1*Random.Range(0.8f,1.2f)).SetEase(Ease.OutBounce).SetUpdate(UpdateType.Normal, true);
+        model.DOScale(scale,1.5f*Random.Range(0.8f,1.2f)).SetEase(Ease.OutElastic).SetDelay(2f).SetUpdate(UpdateType.Normal, true);
+        model.DOLocalMoveY(position.y, 1*Random.Range(0.8f,1.2f)).SetEase(Ease.OutBounce).SetDelay(2f).SetUpdate(UpdateType.Normal, true);
     }
 
     void OnTriggerEnter(Collider other)
